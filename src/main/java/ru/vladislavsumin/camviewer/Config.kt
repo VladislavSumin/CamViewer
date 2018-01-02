e6090d3a04010a3c9c6462b7ea11fc86498aee8c
VladislavSumin <vladislavsumin@mail.ru>
2018-01-02 23:05:57 +0300

    Add logger configuration file Add logging on MainWindow and Config classes

--- src/main/java/ru/vladislavsumin/camviewer/Config.kt
+++ src/main/java/ru/vladislavsumin/camviewer/Config.kt
@@ -1,20 +1,27 @@
 package ru.vladislavsumin.camviewer
 
 import org.json.JSONObject
+import org.slf4j.LoggerFactory
 import java.io.*
 
+/**
+ * @author Vladislav Sumin
+ */
 class Config {
     companion object {
         private const val CONFIG_PATH = "config.json"
-
+        private val log = LoggerFactory.getLogger(Config::class.java)
         private var config: JSONObject = JSONObject()
 
         init {
+            log.info("Try to load config from {}", CONFIG_PATH)
             try {
                 val input = FileReader(CONFIG_PATH)
                 config = JSONObject(input.readText())
                 input.close()
+                log.info("Config loaded")
             } catch (_: FileNotFoundException) {
+                log.warn("Can not load config from {}. Creating default config", CONFIG_PATH)
                 createDefaultConfig()
             }
         }
@@ -26,8 +33,13 @@ class Config {
             val writer = FileWriter(CONFIG_PATH)
             writer.write(config.toString())
             writer.close()
+            log.info("Default config created")
         }
 
-        fun getString(key: String) = config.getString(key)!!
+        /**
+         * @param key - config parameter
+         * @return config string value
+         */
+        fun getString(key: String): String = config.getString(key)!!
     }
 }
--- src/main/java/ru/vladislavsumin/camviewer/gui/MainWindow.kt
+++ src/main/java/ru/vladislavsumin/camviewer/gui/MainWindow.kt
@@ -7,27 +7,36 @@ import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
 import javafx.scene.Scene
 import javafx.stage.Stage
+import org.slf4j.LoggerFactory
 
 /**
  * Main enter point
  */
 class MainWindow : Application() {
     override fun start(root: Stage) {
+        Thread.currentThread().name = "GUI"
+
         val resource = javaClass.classLoader.getResource("MainWindow.fxml")
         val panel: Parent = FXMLLoader.load(resource)
         root.scene = Scene(panel)
+
         root.title = "Cam viewer"
         root.onCloseRequest = EventHandler {
+            log.info("onCloseRequest received")
             Platform.exit()
             System.exit(0)
         }
         root.isMaximized = true
+
         root.show()
     }
 
     companion object {
+        private val log = LoggerFactory.getLogger(MainWindow::class.java)
+
         @JvmStatic
         fun main(args: Array<String>) {
+            log.info("==== MAIN ENTER POINT ====")
             launch(MainWindow::class.java)
         }
     }
